29bbbddf3c06d7888193890040fad05ccae7e039
JamesWiehe <wiehejames@gmail.com>
2024-03-27 15:05:03 +0000

    added_background

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,3 +1,3 @@
 cd /var/www/myapp
 source ~/.bash_profile
-npm start > output.log 2>&1
+npm start
--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -1,5 +1,4 @@
 cd /var/www/myapp
-touch output.log
 source ~/.bash_profile
 nvm install 16
 sudo chown -R $USER /var/www/myapp
